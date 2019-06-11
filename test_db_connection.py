33478944555811c539137be94a1387393cfec289
WillForan <willforan@gmail.com>
2019-06-11 12:22:46 -0400

    WIP: testing with test-pgsql

--- test_db_connection.py
+++ test_db_connection.py
@@ -1,10 +1,16 @@
 import configparser
 import psycopg2
 import psycopg2.sql
+import lncdSql
 # import pyesql
 
 
 def test_read_config_and_connect():
+    """
+    tests read db in config.ini
+    useful to test where code will actually run
+    """
+    # TODO: if cannot read config.ini: pass
     cfg = configparser.ConfigParser()
     cfg.read("config.ini")
     constr = 'dbname=%(dbname)s user=%(user)s ' + \
@@ -12,3 +18,13 @@ def test_read_config_and_connect():
     conn = psycopg2.connect(constr % cfg._sections['SQL'])
     conn.set_session(autocommit=True)
     assert conn is not None
+
+
+def test_fakedb(transacted_postgresql_db):
+    # conn = postgresql_db.engine.connect()
+
+    # AttributeError: 'Connection' object has no attribute 'cursor'
+    conn = transacted_postgresql_db.connection
+    sql = lncdSql.lncdSql(None, conn=conn)
+    sql.query.list_ras()
+
