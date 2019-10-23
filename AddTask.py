5338690d057d89ea582c7b1a8a503eace96f6abd
apeiffer <apeiffer11@gmail.com>
2019-10-23 15:59:03 -0400

    (#36) able to add file path using dialog, still need to connect to db

--- AddTask.py
+++ AddTask.py
@@ -1,5 +1,5 @@
 import json
-import FileDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
 from PyQt5 import uic, QtWidgets
 # from LNCDutils import  *
 
@@ -8,22 +8,23 @@ class AddTaskWindow(QtWidgets.QDialog):
     """
     This class provides a window for adding a Task
     """
+    # Todo:
+    # connect to database (add to self.task_data)
+    # way to delete files after they are added to the list?
+    # optional stuff outlined in issue #37
 
     def __init__(self, parent=None):
         super(AddTaskWindow, self).__init__(parent)
         self.task_data = {}
         uic.loadUi('./ui/add_task.ui', self)
         self.setWindowTitle('Add Task')
-        self.FileDialog = FileDialog.FileDialogWindow()
-
 
         self.task_text.textChanged.connect(self.task)
         self.measures_text.textChanged.connect(self.measures)
         self.modes_text.textChanged.connect(self.modes)
 
+        self.browse_button.clicked.connect(self.addFilePath)
         self.add_file_button.clicked.connect(self.addFile)
-        self.add_file_button.clicked.connect(self.addFileTree) # This should be embedded in the AddTask window
-
 
     def task(self):
         self.task_data['task'] = self.task_text.text()
@@ -41,11 +42,12 @@ class AddTaskWindow(QtWidgets.QDialog):
         self.file_id_list.append(file_id)
         self.file_id_text.clear()
 
-        # Backup way to add file locations if QFileDialog does not work
-        """
         file_loc = self.file_loc_text.text()
         self.file_loc_list.append(file_loc)
         self.file_loc_text.clear()
-        """
-    def addFileTree(self):
-        self.FileDialog.initUI()
+
+    def addFilePath(self):
+        file_loc = self.file_loc_text.text()
+        file_path = str(QFileDialog.getOpenFileName(parent=self, directory=file_loc)[0])
+        if file_path:
+            self.file_loc_text.setText(file_path)
