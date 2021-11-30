dc8a8b581402e464fb1939dc87b51e6cb9a6b678
WillForan <willforan@gmail.com>
2021-11-30 11:57:18 -0500

    🐛run.bash: install hook instead of echoing install cmd

--- run.bash
+++ run.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 # install git hook if not already there
-test ! -r .git/hooks/pre-commit && ( cd  .git/hooks/; echo ln -s ../../hooks/pre-commit ./; )
+test ! -r .git/hooks/pre-commit && ( cd  .git/hooks/; ln -s ../../hooks/pre-commit ./; )
 pipenv sync
 pipenv run ./schedule.py
