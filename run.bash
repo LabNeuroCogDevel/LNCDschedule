1b647fdda27456eeca9f99bd08b98a03d81aa988
Will Foran <willforan@gmail.com>
2021-11-30 11:54:10 -0500

    🔨 run.bash installs hook; hook also runs black

--- run.bash
+++ run.bash
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -e
+# install git hook if not already there
+test ! -r .git/hooks/pre-commit && ( cd  .git/hooks/; echo ln -s ../../hooks/pre-commit ./; )
 pipenv sync
 pipenv run ./schedule.py
